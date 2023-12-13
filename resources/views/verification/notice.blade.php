e2062f5686959b1fccf996824d7e77edea7d36b2
aryadg <aryagaharu.yt@gmail.com>
2023-12-13 22:32:50 +0700

    update notice verify register

--- resources/views/verification/notice.blade.php
+++ resources/views/verification/notice.blade.php
@@ -1,12 +1,43 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Kamu belum verifikasi email</h1>
-</body>
-</html>
+@extends('layouts.auth')
+
+@section('title', 'Verify Email')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
+@endpush
+
+@section('main')
+    <div class="card card-primary">
+        <div class="card-header">
+            <h4>Please check your email for verification</h4>
+        </div>
+        @if (session('status') == 'verification-link-sent')
+            <div class="mb-4 font-medium text-sm text-green-600">
+                A new email verification link has been emailed to you!
+            </div>
+        @endif
+
+        <div class="card-body">
+            <form method="POST" action="{{ route('verification.send') }}" class="needs-validation" novalidate="">
+                @csrf
+                <div class="form-group">
+                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                        Resend Verification Email
+                    </button>
+                </div>
+            </form>
+
+
+        </div>
+    </div>
+    <div class="text-muted mt-5 text-center">
+        Don't have an account? <a href="bd-register">Create One</a>
+    </div>
+@endsection
+
+@push('scripts')
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+@endpush
